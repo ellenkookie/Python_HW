0e5e0ac10750a0e0cab81d4dda82d1146e4aaab5
Elen Hayrapetyan <elenhayro2000@gmail.com>
2026-01-29 23:46:32 +0300

    lesson2 - 7 tasks

--- 02_lesson/lesson_2_task_2.py
+++ 02_lesson/lesson_2_task_2.py
@@ -1,10 +1,11 @@
-#Високосный год
+# Високосный год
 
 def is_year_leap(year):
     if year % 4 == 0:
         return True
     else:
         return False
+
 
 year1 = 2024
 result1 = is_year_leap(year1)
