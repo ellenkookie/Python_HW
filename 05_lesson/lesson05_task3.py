3b7db9c5081ed7ea6e1c401e77634f69f5779b1a
Elen Hayrapetyan <elenhayro2000@gmail.com>
2026-02-23 20:35:50 +0300

    Pytest lesson_05

---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,22 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+sleep(2)
+input_field = driver.find_element(By.TAG_NAME, "input")
+input_field.send_keys("Sky")
+
+sleep(2)
+input_field.clear()
+
+sleep(2)
+input_field.send_keys("Pro")
+
+sleep(5)
+driver.quit()
