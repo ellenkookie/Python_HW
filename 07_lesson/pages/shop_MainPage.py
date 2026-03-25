83c029b2caecf01f6744d2e5ed6fbe12d8c80573
Elen Hayrapetyan <elenhayro2000@gmail.com>
2026-03-26 01:00:28 +0300

    Page Object Model (урок 7)
    ✅ Выполнены автотесты для 3 сценариев с использованием POM.
    
    Стек:
    - Selenium
    - pytest
    - Page Object Model
    
    Особенности:
    - явные ожидания
    - фикстура для драйвера
    - тесты не содержат Selenium-методов

--- 07_lesson/pages/shop_MainPage.py
+++ 07_lesson/pages/shop_MainPage.py
@@ -1,11 +1,14 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class MainPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def add_to_cart(self, product_id: str):
-        self.driver.find_element(By.ID, product_id).click()
+        self.wait.until(EC.element_to_be_clickable((By.ID, product_id))).click()
 
     def go_to_cart(self):
-        self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+        self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link"))).click()
