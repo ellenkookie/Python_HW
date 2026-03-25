9e233f513e8d59b061b558181093f086e7d39796
Elen Hayrapetyan <elenhayro2000@gmail.com>
2026-03-26 00:39:47 +0300

    Page Object Model (урок 7)
    ✅ Выполнены автотесты для 3 сценариев с использованием POM.
    
    Стек:
    - Selenium
    - pytest
    - Page Object Model
    
    Особенности:
    - явные ожидания
    - фикстура для драйвера
    - тесты не содержат Selenium-методов

--- 07_lesson/pages/shop_LoginPage.py
+++ 07_lesson/pages/shop_LoginPage.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
     def __init__(self, driver):
@@ -10,6 +11,8 @@ class LoginPage:
         self.driver.get("https://www.saucedemo.com/")
 
     def login(self, username: str, password: str):
-        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.wait.until(
+            EC.visibility_of_element_located((By.ID, "user-name"))
+        ).send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
         self.driver.find_element(By.ID, "login-button").click()
